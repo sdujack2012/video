ac962cd024404dbffd4e63370b932b4db4dc12c6
Kai <sdujack2012@gmail.com>
2024-05-23 22:02:55 +1000

    fix some bugs

---
+++ create_stories.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const path = require("path");
+const { createFolderIfNotExist } = require("./utils");
+const {
+  generateShortVideoResources,
+} = require("./generate_story_resources");
+const { renderShortVideo } = require("./render_video");
+
+async function createShortStories() {
+  const storiesPath = path.resolve("stories.json");
+  const stories = JSON.parse(fs.readFileSync(storiesPath, "utf8"));
+  console.log(stories);
+  for (let story of stories) {
+    const storyFolder = createFolderIfNotExist("short_story", story.title);
+    const storyJsonPath = path.resolve(storyFolder, "story.json");
+    if (!fs.existsSync(storyJsonPath)) {
+      fs.writeFileSync(storyJsonPath, JSON.stringify(story));
+    }
+    await generateShortVideoResources(story.title);
+    const existingStory =  JSON.parse(fs.readFileSync(storyJsonPath, "utf8"));
+    if (fs.existsSync(existingStory.videoFile)) {
+      continue;
+    }
+    await renderShortVideo(story.title);
+  }
+}
+
+if (require.main === module) {
+  createShortStories();
+}
